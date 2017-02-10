6f49895724110cbd3b244bf982d9cf260dd1c9d9
leeadh <leexhadrian@gmail.com>
2017-02-10 15:32:04 +0800

    Update getMovieData.js

--- getMovieData.js
+++ getMovieData.js
@@ -1,21 +1,23 @@
 const APIkey = '27a78d4369e4cb5dc2ca8a5640545ead';
 const rp = require('request-promise');
+const async = require('async');
 const MovieDB = require('moviedb')(APIkey);
 const userAgentHeader = {
-	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
+	'User-Agent': 'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/24.0',
 	'Content-Type': 'application/x-www-form-urlencoded'
 };
 
 //(to get image poster)https://image.tmdb.org/t/p/w640/<poster_path>
 // to get youtube link https://www.youtube.com/watch?v=<key>
 
-
+var returnVal;
 function getMovieInformation(movieName, year) {
-	var returnVal;
+	
 	var options = {
-		url: "http://www.omdbapi.com/?t=" + movieName + "&y=" + year + "&plot=short&tomatoes=true&r=json",
+		url: "http://www.omdbapi.com/?t=" + movieName + "&y=" + year + "&tomatoes=true&plot=shorte&r=json",
 		headers: userAgentHeader,
-		json: true
+		json: true,
+		timeout:3000000
 	};
 
 	rp(options)
@@ -40,25 +42,5 @@ function getMovieInformation(movieName, year) {
 	});
 
 }
-/*
-function getYoutubeTrailer(imdbID) {
-	var youtubeURL;
-	var options = {
-		url: "https://api.themoviedb.org/3/find/" + imdbID + "?api_key=" + APIkey + "&language=en-US&external_source=imdb_id",
-		headers: userAgentHeader,
-		json: true
 
-	};
 
-	return rp(options)
-	.then(function (repos) {
-		MovieDB.movieTrailers({
-			id: repos.movie_results[0].id
-		}, function (err, res) {
-			youtubeURL = "https://www.youtube.com/watch?v=" + res.youtube[0].source;
-			return youtubeURL;
-		});
-	});
-
-}*/
-getMovieInformation("Split", "2016");
